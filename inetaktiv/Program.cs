89641e56bb23a7d1de91f3f63b4d06d9d49798a9
joakim lundell selander <joakim.lundellselander@elev.ga.ntig.se>
2024-09-13 09:49:14 +0200

    hgfjgf

--- inetaktiv/Program.cs
+++ inetaktiv/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
+﻿
+Console.WriteLine("du blir jagad av 7 poliser med din trimmade moppe på en motor väg och du komer framm till en korstning villken väg väljer du eller väljer du att stanna och morsan som drar in ditt fortnite konto");
+string väg = Console.ReadLine();
+
+if (väg ==  "höger")
+{
+Console.WriteLine("du kom undan");
+Console.ReadLine();
+}
+else if (väg == "vänster")
+{
+Console.WriteLine("du blir på körd av 15 lastbilar och blir brutalt mördad ");
+}
+
+//if ()
